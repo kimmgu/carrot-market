d43a43efb84028add932a3769282651d65522f49
Kim Mingu <MINGU757377@gmail.com>
2022-10-16 23:50:39 +0900

    Create account logic

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,21 +1,4 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
-
-// export default function withHandler(
-//   method: 'GET' | 'POST' | 'DELETE',
-//   fn: (req: NextApiRequest, res: NextApiResponse) => void
-// ) {
-//   return async function (req: NextApiRequest, res: NextApiResponse) {
-//     if (req.method !== method) {
-//       return res.status(405).end()
-//     }
-//     try {
-//       await fn(req, res)
-//     } catch (error) {
-//       console.log(error)
-//       return res.status(500).json({ error })
-//     }
-//   }
-// }
+import { NextApiHandler } from 'next'
 
 export const withHandler = (
   method: 'GET' | 'POST' | 'DELETE',
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,60 @@
+import client from '@libs/server/client'
 import { withHandler } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body)
+  const { email, phone } = req.body
+  const payload = phone ? { phone: +phone } : { email }
+  const user = await client.user.upsert({
+    where: { ...payload },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  })
+  console.log(user)
+  //  let user
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   })
+  //   if (user) {
+  //     console.log('User found')
+  //   }
+  //   if (!user) {
+  //     console.log('User not found')
+  //     user = await client.user.create({
+  //       data: {
+  //         name: 'Anonymous',
+  //         email,
+  //       },
+  //     })
+  //   }
+  //   console.log(user)
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   })
+  //   if (user) {
+  //     console.log('User found')
+  //   }
+  //   if (!user) {
+  //     console.log('User not found')
+  //     user = await client.user.create({
+  //       data: {
+  //         name: 'Anonymous',
+  //         phone: +phone,
+  //       },
+  //     })
+  //   }
+  //   console.log(user)
+  // }
   return res.status(200).end()
 }
 
