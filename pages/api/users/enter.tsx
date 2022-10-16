68538ac85926c196ca36fac2802320dbd329f71e
Kim Mingu <MINGU757377@gmail.com>
2022-10-17 01:03:28 +0900

    Refactoring handler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,14 @@
 import client from '@libs/server/client'
-import { withHandler } from '@libs/server/withHandler'
+import { ResponseType, withHandler } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = req.body
-  const user = phone ? { phone: +phone } : { email }
+  const user = phone ? { phone: +phone } : email ? { email } : null
+  if (!user) return res.status(400).json({ ok: false, error: 'Bad Request' })
   const payload = Math.floor(100000 + Math.random() * 900000) + ''
   const token = await client.token.create({
     data: {
@@ -20,8 +24,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   })
-  console.log(token)
-  return res.status(200).end()
+  return res.json({
+    ok: true,
+  })
 }
 
 export default withHandler('POST', handler)
