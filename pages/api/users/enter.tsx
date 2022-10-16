a39bd5b6a1044efbc5fa76b8002e77f6cc177abc
Kim Mingu <MINGU757377@gmail.com>
2022-10-17 01:24:25 +0900

    SMS Verification

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,12 @@
 import client from '@libs/server/client'
 import { ResponseType, withHandler } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
+import twilio from 'twilio'
+
+const twilioClient = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+)
 
 async function handler(
   req: NextApiRequest,
@@ -24,6 +30,15 @@ async function handler(
       },
     },
   })
+  if (phone) {
+    const msg = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+      // to: phone,
+      to: process.env.MY_PHONE_NUMBER!,
+      body: `Your verification code is ${payload}`,
+    })
+    console.log(msg)
+  }
   return res.json({
     ok: true,
   })
