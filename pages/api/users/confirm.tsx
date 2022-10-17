3f549fdb4b13106f6b431b816f3c1d053c6f3fde
Kim Mingu <MINGU757377@gmail.com>
2022-10-18 00:20:21 +0900

    Token confirm features

---
+++ pages/api/users/confirm.tsx
@@ -0,0 +1,13 @@
+import { ResponseType, withHandler } from '@libs/server/withHandler'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { token } = req.body
+  console.log(token)
+  res.status(200).end()
+}
+
+export default withHandler('POST', handler)
