6ebb0cb90588f11aad323902e29f89c7c8e0cd39
Kim Mingu <MINGU757377@gmail.com>
2022-10-19 18:34:58 +0900

    Create favorite feature

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,5 +1,7 @@
 import Button from '@components/button'
 import Layout from '@components/layout'
+import useMutation from '@libs/client/useMutation'
+import { cls } from '@libs/client/utils'
 import { Product, User } from '@prisma/client'
 import { NextPage } from 'next'
 import Link from 'next/link'
@@ -14,6 +16,7 @@ interface ItemDetailResponse {
   ok: boolean
   product: ProductWithUser
   relatedProducts: Product[]
+  isFavorited: boolean
 }
 
 const ItemDetail: NextPage = () => {
@@ -21,6 +24,12 @@ const ItemDetail: NextPage = () => {
   const { data } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   )
+  const [toggleFavorite] = useMutation(
+    `/api/products/${router.query.id}/favorite`
+  )
+  const onFavoriteClick = () => {
+    toggleFavorite({})
+  }
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
@@ -33,8 +42,22 @@ const ItemDetail: NextPage = () => {
                 {data?.product?.user?.name}
               </p>
               <Link href={`/users/profiles/${data?.product?.user?.id}`}>
-                <a className="text-xs font-medium text-gray-500">
-                  판매자 정보 &rarr;
+                <a className="text-xs font-medium text-gray-500 flex">
+                  판매자 정보
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 -4 12 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
                 </a>
               </Link>
             </div>
@@ -49,22 +72,40 @@ const ItemDetail: NextPage = () => {
             <p className=" my-6 text-gray-700">{data?.product?.description}</p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="채팅하기" />
-              <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
-                <svg
-                  className="h-6 w-6 "
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
+              <button
+                onClick={onFavoriteClick}
+                className={cls(
+                  'p-3 rounded-md flex items-center justify-center hover:bg-gray-100',
+                  data?.isFavorited
+                    ? 'text-orange-500  hover:text-orange-600'
+                    : 'text-gray-500  hover:text-gray-600'
+                )}
+              >
+                {data?.isFavorited ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
