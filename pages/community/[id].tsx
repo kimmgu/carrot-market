3fc642328b1355eb85b7e6f63e42eff0630a70f5
Kim Mingu <MINGU757377@gmail.com>
2022-10-21 01:13:12 +0900

    Community answers feature

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -6,6 +6,8 @@ import { Answer, Post, User } from '@prisma/client'
 import { NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
 
 interface AnswerWithUser extends Answer {
@@ -27,12 +29,23 @@ interface CommunityPostResponse {
   isCuriosity: boolean
 }
 
+interface AnswerForm {
+  answer: string
+}
+
+interface AnswerResponse {
+  ok: boolean
+  respnse: Answer
+}
+
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter()
   const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   )
-  const [curiosity] = useMutation(`/api/posts/${router.query.id}/curiosity`)
+  const [curiosity, { loading }] = useMutation(
+    `/api/posts/${router.query.id}/curiosity`
+  )
   const onCuriosityClick = () => {
     if (!data) return
     mutate(
@@ -51,8 +64,22 @@ const CommunityPostDetail: NextPage = () => {
       },
       false
     )
-    curiosity({})
+    if (!loading) {
+      curiosity({})
+    }
   }
+  const [sendAnswer, { data: answerData, loading: answerLoading }] =
+    useMutation<AnswerResponse>(`/api/posts/${router.query.id}/answers`)
+  const { register, handleSubmit, reset } = useForm<AnswerForm>()
+  const onValid = (form: AnswerForm) => {
+    if (answerLoading) return
+    sendAnswer(form)
+  }
+  useEffect(() => {
+    if (answerData && answerData.ok) {
+      reset()
+    }
+  }, [answerData, reset])
   return (
     <Layout canGoBack>
       <div>
@@ -144,23 +171,25 @@ const CommunityPostDetail: NextPage = () => {
                   {answer.user.name}
                 </span>
                 <span className="text-xs text-gray-500 block ">
-                  {answer.createdAt.toString()}
+                  {/* {answer.createdAt} */}
                 </span>
                 <p className="text-gray-700 mt-2">{answer.answer}</p>
               </div>
             </div>
           ))}
         </div>
-        <div className="px-4">
+
+        <form onSubmit={handleSubmit(onValid)} className="px-4">
           <TextArea
             name="description"
             placeholder="댓글을 입력해주세요."
             required
+            register={register('answer', { required: true, minLength: 10 })}
           />
           <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            답변하기
+            {answerLoading ? '로딩중...' : '답변하기'}
           </button>
-        </div>
+        </form>
       </div>
     </Layout>
   )
