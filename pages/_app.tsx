d50588b98a8e9df1cf640bc6f26a8d798d80193f
Kim Mingu <MINGU757377@gmail.com>
2022-10-22 03:01:01 +0900

    Make sure logged in globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import useUser from '@libs/client/useUser'
+
+function CheckUser() {
+  useUser()
+  return null
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       value={{ fetcher: (url: string) => fetch(url).then((res) => res.json()) }}
     >
       <div className="w-full max-w-xl mx-auto">
+        <CheckUser />
         <Component {...pageProps} />
       </div>
     </SWRConfig>
