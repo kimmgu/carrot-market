2038a3b4f10a54dbbf29f45e807f31d82a319883
Kim Mingu <MINGU757377@gmail.com>
2022-10-23 23:24:44 +0900

    Create streams API handler

---
+++ pages/api/streams/[id].ts
@@ -0,0 +1,21 @@
+import { ResponseType, withHandler } from '@libs/server/withHandler'
+import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/server/client'
+import { withApiSession } from '@libs/server/withSession'
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req
+  const stream = await client.stream.findUnique({
+    where: {
+      id: +id!.toString(),
+    },
+  })
+  res.json({ ok: true, stream })
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }))
