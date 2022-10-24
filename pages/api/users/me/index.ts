4903fdcead14f1a52c22dbbafea69967b64bc9fb
Kim Mingu <MINGU757377@gmail.com>
2022-10-24 23:32:18 +0900

    Profile photo cloudflare connection

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -16,7 +16,7 @@ async function handler(
   if (req.method === 'POST') {
     const {
       session: { user },
-      body: { name, email, phone },
+      body: { avatarId, name, email, phone },
     } = req
     const currentUser = await client.user.findUnique({
       where: { id: user?.id },
@@ -58,6 +58,12 @@ async function handler(
         data: { phone },
       })
       res.json({ ok: true })
+    }
+    if (avatarId) {
+      await client.user.update({
+        where: { id: user?.id },
+        data: { avatar: avatarId },
+      })
     }
     res.json({ ok: true })
   }
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -36,6 +36,10 @@ const EditProfile: NextPage = () => {
     if (user?.name) setValue('name', user.name)
     if (user?.email) setValue('email', user.email)
     if (user?.phone) setValue('phone', user.phone)
+    if (user?.avatar)
+      setAvatarPreview(
+        `https://imagedelivery.net/aSbksvJjax-AUC7qVnaC4A/${user?.avatar}/public`
+      )
   }, [user, setValue])
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`)
@@ -47,21 +51,22 @@ const EditProfile: NextPage = () => {
       })
     }
     if (avatar && avatar.length > 0) {
-      // ask for cloudflare url
-      const { id, uploadURL } = await (await fetch('/api/files')).json()
+      const { uploadURL } = await (await fetch('/api/files')).json()
       const form = new FormData()
       form.append('file', avatar[0], user?.id + '')
-      await fetch(uploadURL, {
-        method: 'POST',
-        body: form,
-      })
-      // upload file to cloudflare url
-      return
+      const {
+        result: { id },
+      } = await (
+        await fetch(uploadURL, {
+          method: 'POST',
+          body: form,
+        })
+      ).json()
       editProfile({
         name,
         email,
         phone,
-        // avatarUrl: cloudflareUrl
+        avatarId: id,
       })
     } else {
       editProfile({
